a0225cd10a3dc1c36f416546cd3440c5dbd09d15
Peter Gani <peter.gani@live.com>
2019-02-13 18:39:20 -0500

    Solution - Sock Merchant

--- Interview Preparation Kit/Warmup Challenges/Sock Merchant/Sock Merchant/Program.cs
+++ Interview Preparation Kit/Warmup Challenges/Sock Merchant/Sock Merchant/Program.cs
@@ -14,7 +14,6 @@ using System;
 
 class Solution
 {
-
     // Complete the sockMerchant function below.
     static int sockMerchant(int n, int[] ar)
     {
@@ -29,18 +28,19 @@ class Solution
             if (ar[i] != -1)
             {
                 counter = 0;
+                color = ar[i];
                 for (int j = 0; j < ar.Length; j++)
                 {
-                    if (ar[i] == ar[j])
+                    if (color == ar[j])
                     {
                         ar[j] = -1;
                         counter++;
                     }
-
-                    //Add up number of pairs
-                    counter /= 2;
-                    pairs += counter;
                 }
+
+                //Add up number of pairs
+                counter /= 2;
+                pairs += counter;
             }
         }
         return pairs;
@@ -48,17 +48,29 @@ class Solution
 
     static void Main(string[] args)
     {
+        /*
         TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
         int n = Convert.ToInt32(Console.ReadLine());
 
         int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+        */
+
+        int n = 10;
+
+        int[] ar = { 1, 1, 3, 1, 2, 1, 3, 3, 3, 3 };
+
         ;
         int result = sockMerchant(n, ar);
 
+        /*
         textWriter.WriteLine(result);
 
         textWriter.Flush();
         textWriter.Close();
+        */
+
+        Console.WriteLine(result);
+        Console.ReadLine();
     }
 }
