cd573d2a05465ec60a02e7863c3b9f1b3b29a573
Peter Gani <peter.gani@live.com>
2019-02-13 20:54:47 -0500

    Solution - Jumping on the Clouds

--- Interview Preparation Kit/Warmup Challenges/Jumping on the Clouds/Jumping on the Clouds/Program.cs
+++ Interview Preparation Kit/Warmup Challenges/Jumping on the Clouds/Jumping on the Clouds/Program.cs
@@ -18,8 +18,27 @@ class Solution
     // Complete the jumpingOnClouds function below.
     static int jumpingOnClouds(int[] c)
     {
+        //variables
+        int minJumps = 0;
 
+        //Go through clouds
+        for (int i = 0; i < c.Length; i++)
+        {
+            //Check if near the end of the arraay;
+            if (i <= c.Length - 2)
+            {
+                //If the next two spots is safe, jump there
+                if (i < c.Length - 2 && c[i + 2] == 0)
+                {
+                    //Move an extra position
+                    i++;
+                }
+                //Add minimum jumps
+                minJumps++;
+            }
+        }
 
+        return minJumps;
     }
 
     static void Main(string[] args)
